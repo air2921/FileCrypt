8f6b6bc7b26d0ff95e6ebcfd339cc29a87508a06
Yan Dekov <StewieSolden147@gmail.com>
2023-06-10 12:08:25 +0700

    Создан класс для управления директориями и файлами внутри них

---
+++ FileCrypt/DirectoryAndFileOperations.cs
@@ -0,0 +1,72 @@
+﻿using System.Diagnostics;
+
+namespace FileCrypt
+{
+    internal class DirectoryAndFileOperations : IDirectoryOperations
+    {
+        public void DeleteDirectory(string directoryPath)
+        {
+            try
+            {
+                Process process = new Process();
+                process.StartInfo.FileName = "cmd.exe";
+                process.StartInfo.Arguments = $"/c rd /s /q \"{directoryPath}\"";
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                process.Start();
+                process.WaitForExit();
+                Console.WriteLine($"Директория {directoryPath} успешно удалена.");
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                Console.WriteLine($"Ошибка удаления директории {directoryPath}: отсутствуют необходимые разрешения доступа.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Непредвиденная ошибка при попытке удаления директории {ex.Message}");
+            }
+        }
+        public void CreateBackup(string sourceDirectory, string backupDirectory)
+        {
+            CreateDirectory(backupDirectory);
+
+            try
+            {
+                string[] files = Directory.GetFiles(sourceDirectory);
+                foreach (string file in files)
+                {
+                    string fileName = Path.GetFileName(file);
+                    string destPath = Path.Combine(backupDirectory, fileName);
+                    File.Copy(file, destPath, true);
+                }
+
+                string[] directories = Directory.GetDirectories(sourceDirectory);
+                foreach (string directory in directories)
+                {
+                    string directoryName = Path.GetFileName(directory);
+                    string destPath = Path.Combine(backupDirectory, directoryName);
+                    CreateBackup(directory, destPath);
+                }
+                Console.WriteLine("Резервная копия была создана успешно");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при создании резервной копии директории: {ex.Message}");
+            }
+            //var backupDirectory = $"C:/{sourceDirectory}(Reserve)";
+        }
+
+        private static void CreateDirectory(string backupDirectory)
+        {
+            if (!Directory.Exists(backupDirectory))
+            {
+                Directory.CreateDirectory(backupDirectory);
+            }
+        }
+    }
+    public interface IDirectoryOperations
+    {
+        void CreateBackup(string sourceDirectory, string backupDirectory);
+        void DeleteDirectory(string directoryPath);
+    }
+}
