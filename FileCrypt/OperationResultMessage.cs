b8556e6a3d9da63c556ce029a9153d6c431f4987
Yan Dekov <StewieSolden147@gmail.com>
2023-06-13 10:38:22 +0700

    Добавил отдельный класс для вывода сообщений о результате выполнения операции шифрование/расшифровки

---
+++ FileCrypt/OperationResultMessage.cs
@@ -0,0 +1,28 @@
+﻿namespace FileCrypt
+{
+    internal class OperationResultMessage
+    {
+        public void ResultMessage(int allFiles, int totalFiles)
+        {
+            if (allFiles == totalFiles && totalFiles > 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\nВсе файлы в директории были успешно обработаны.");
+                Console.ResetColor();
+            }
+            else if (allFiles == 0 && totalFiles == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nВ директории не найдено файлов для выполнения операции или при выполнении операции произошла ошибка.");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nНе все файлы удалось успешно обработать.");
+                Console.ResetColor();
+            }
+            Console.ReadKey();
+        }
+    }
+}
