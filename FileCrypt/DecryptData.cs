af28fda21e195e465ceab1a8f683b1f3b3df6cb6
Yan Dekov <StewieSolden147@gmail.com>
2023-06-16 05:34:48 +0700

    Методы переписаны на более современный C#, немнного оптимизирован код, уменьшено кол-во итераций

--- FileCrypt/DecryptData.cs
+++ FileCrypt/DecryptData.cs
@@ -6,21 +6,17 @@ namespace FileCrypt
     {
         private static void Decrypt(Stream source, Stream target, byte[] key)
         {
-            using (Aes aes = Aes.Create())
+            using var aes = Aes.Create();
+            byte[] iv = new byte[aes.BlockSize / 8];
+            source.Read(iv);
+            aes.IV = iv;
+            using (Rfc2898DeriveBytes rfc2898 = new(key, iv, 1000, HashAlgorithmName.SHA256))
             {
-                byte[] iv = new byte[aes.IV.Length];
-                source.Read(iv, 0, iv.Length);
-                aes.IV = iv;
-                using (Rfc2898DeriveBytes rfc2898 = new Rfc2898DeriveBytes(key, iv, 10000))
-                {
-                    aes.Key = rfc2898.GetBytes(aes.KeySize / 8);
-                }
-
-                using (CryptoStream cryptoStream = new CryptoStream(source, aes.CreateDecryptor(), CryptoStreamMode.Read))
-                {
-                    cryptoStream.CopyTo(target);
-                }
+                aes.Key = rfc2898.GetBytes(aes.KeySize / 8);
             }
+
+            using CryptoStream cryptoStream = new(source, aes.CreateDecryptor(), CryptoStreamMode.Read);
+            cryptoStream.CopyTo(target);
         }
 
         public void DecryptFile(string filePath, byte[] key)
--- FileCrypt/EncryptData.cs
+++ FileCrypt/EncryptData.cs
@@ -6,20 +6,16 @@ namespace FileCrypt
     {
         private static void Encrypt(Stream source, Stream target, byte[] key)
         {
-            using (Aes aes = Aes.Create())
+            using var aes = Aes.Create();
+            byte[] iv = aes.IV;
+            target.Write(iv);
+            using (Rfc2898DeriveBytes rfc2898 = new(key, iv, 1000, HashAlgorithmName.SHA256))
             {
-                byte[] iv = aes.IV;
-                target.Write(iv, 0, iv.Length);
-                using (Rfc2898DeriveBytes rfc2898 = new Rfc2898DeriveBytes(key, iv, 10000))
-                {
-                    aes.Key = rfc2898.GetBytes(aes.KeySize / 8);
-                }
-
-                using (CryptoStream cryptoStream = new CryptoStream(target, aes.CreateEncryptor(), CryptoStreamMode.Write))
-                {
-                    source.CopyTo(cryptoStream);
-                }
+                aes.Key = rfc2898.GetBytes(aes.KeySize / 8);
             }
+
+            using CryptoStream cryptoStream = new(target, aes.CreateEncryptor(), CryptoStreamMode.Write);
+            source.CopyTo(cryptoStream);
         }
 
         public void EncryptFile(string filePath, byte[] key)
