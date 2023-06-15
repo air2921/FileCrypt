426ac0420e2dd24ef0c219e9b2ef5ba902442dd7
Yan Dekov <StewieSolden147@gmail.com>
2023-06-15 11:20:43 +0700

    Добавил бесконечный цикл, чтобы после выполнения каждой команды не перезаходить в приложение.

--- FileCrypt/Program.cs
+++ FileCrypt/Program.cs
@@ -9,52 +9,58 @@
 
             Console.WriteLine("Введите команду которую хотите выполнить");
             string InputUser = Console.ReadLine();
-            switch (InputUser)
+            while (InputUser != "EXIT")
             {
-                case ".help":
-                    command.Help();
+                switch (InputUser)
+                {
+                    case ".help":
+                        command.Help();
 
-                    break;
-                case "GENERATE":
-                    command.Generate();
+                        break;
+                    case "GENERATE":
+                        command.Generate();
 
-                    break;
-                case "FENC":
-                    command.EncryptFile();
+                        break;
+                    case "FENC":
+                        command.EncryptFile();
 
-                    break;
-                case "FDEC":
-                    command.DecryptFile();
+                        break;
+                    case "FDEC":
+                        command.DecryptFile();
 
-                    break;
-                case "DIRENC":
-                    command.EncryptDirectory();
+                        break;
+                    case "DIRENC":
+                        command.EncryptDirectory();
 
-                    break;
-                case "DIRDEC":
-                    command.DecryptDirectory();
+                        break;
+                    case "DIRDEC":
+                        command.DecryptDirectory();
 
-                    break;
-                case "DIRDEL":
-                    command.DeleteDirectory();
+                        break;
+                    case "DIRDEL":
+                        command.DeleteDirectory();
 
-                    break;
-                case "DIRBACKUP":
-                    command.CreateBackupDirectory();
+                        break;
+                    case "DIRBACKUP":
+                        command.CreateBackupDirectory();
 
-                    break;
-                case "EX":
-                    command.Example();
-                    
-                    break;
-                default:
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Такой команды не существует.\nВы можете ввести '.help' для получения информации о применяемых командах" +
-                        " и возможных ошибках");
-                    Console.ReadKey();
-                    break;
+                        break;
+                    case "EX":
+                        command.Example();
 
-            }
+                        break;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("\nТакой команды не существует.\nВы можете ввести '.help' для получения справочной информации.");
+                        Console.ResetColor();
+                        break;
+
+                }
+
+                Console.ResetColor();
+                Console.WriteLine("\n\nВведите следующую команду:");
+                InputUser = Console.ReadLine();
+            }    
         }
     }
 }
